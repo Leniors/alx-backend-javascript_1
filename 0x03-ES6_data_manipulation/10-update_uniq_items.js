99dc99c24f1e9f05ec98d77443730f3565edb5aa
Leniors <mokualeeroy@gmail.com>
2024-05-01 03:46:35 +0300

    es6

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 export default function updateUniqueItems(groceriesMap) {
-	  if (!(groceriesMap instanceof Map)) {
-		      throw new Error("Cannot process");
-		    }
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error("Cannot process");
+  }
 
-	  for (const [item, quantity] of groceriesMap.entries()) {
-		      if (quantity === 1) {
-			            groceriesMap.set(item, 100);
-			          }
-		    }
+  for (const [item, quantity] of groceriesMap.entries()) {
+    if (quantity === 1) {
+      groceriesMap.set(item, 100);
+    }
+  }
 
-	  return groceriesMap;
+  return groceriesMap;
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,12 @@
 export default function cleanSet(set, startString) {
-	  if (!startString) {
-		  return '';
-	  }
-	  const filteredValues = [...set].filter(value => value.startsWith(startString));
-	  const cleanedValues = filteredValues.map(value => value.slice(startString.length));
-	  return cleanedValues.join('-');
+  if (!startString) {
+    return "";
+  }
+  const filteredValues = [...set].filter((value) =>
+    value.startsWith(startString),
+  );
+  const cleanedValues = filteredValues.map((value) =>
+    value.slice(startString.length),
+  );
+  return cleanedValues.join("-");
 }
