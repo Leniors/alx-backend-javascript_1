c30286851de8b0348d7e70175df146c32c4b2229
Leniors <mokualeeroy@gmail.com>
2024-05-26 10:41:09 +0300

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
-  try {
     const photoResponse = await uploadPhoto();
     const userResponse = await createUser();
     console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-  } catch (error) {
-    console.error('Error:', error);
-  }
 }
 
 export default handleProfileSignup;
