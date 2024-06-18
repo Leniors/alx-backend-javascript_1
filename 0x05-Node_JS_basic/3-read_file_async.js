4be14aa9eea77b7a4887dea91453d75fde701334
Leniors <mokualeeroy@gmail.com>
2024-06-19 02:08:05 +0300

    async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+    try {
+        const data = await fs.readFile(path, 'utf-8');
+        const lines = data.trim().split('\n');
+
+        if (lines.length <= 1) {
+            throw new Error('Cannot load the database');
+        }
+
+        const students = {};
+        let totalStudents = 0;
+
+        for (let i = 1; i < lines.length; i++) {
+            const line = lines[i].trim();
+            if (line) {
+                const [firstname, lastname, age, field] = line.split(',');
+
+                if (!students[field]) {
+                    students[field] = [];
+                }
+
+                students[field].push(firstname);
+                totalStudents++;
+            }
+        }
+
+        console.log(`Number of students: ${totalStudents}`);
+
+        for (const field in students) {
+            const count = students[field].length;
+            const names = students[field].join(', ');
+            console.log(`Number of students in ${field}: ${count}. List: ${names}`);
+        }
+    } catch (error) {
+        throw new Error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
